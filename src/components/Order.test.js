3fa238bb7cd5f4f07a09125cadc3345c5d9fb29e
Lukasz Winowicz <winowiczlukasz@gmail.com>
2019-01-07 22:10:40 +0100

    Components created

---
+++ src/components/Order.test.js
@@ -0,0 +1,24 @@
+import React from 'React';
+import ReactDOM from 'react-dom';
+import Order from './Order';
+import {configure, shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({adapter: new Adapter()});
+
+describe('Order tests', () => {
+
+
+    it('Order renders without a problem', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<Order />, div);
+        ReactDOM.unmountComponentAtNode(div);
+
+    })
+
+    it('Hello world renders', () => {
+        const wrapper = shallow(<Order />);
+        expect(wrapper.find('div').text()).toBe('Order');
+    })
+
+})
