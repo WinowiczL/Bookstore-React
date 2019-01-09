f227675af5bad0865d347a53b2b1869ba120313a
LukaszWinowicz <lukaszwinowicz.capgemini.com>
2019-01-09 10:55:26 +0100

    form tie

--- src/components/AdminPanel.js
+++ src/components/AdminPanel.js
@@ -1,6 +1,39 @@
 import React from "react";
 
 class AdminPanel extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      book: {
+        bookName: "",
+        bookAuthor: "",
+        bookDescription: "",
+        bookOnStock: false,
+        bookImage: ""
+      }
+    };
+  }
+
+  handleChange = event => {
+    let newBook;
+
+    if (event.target.name === "bookOnStock") {
+      newBook = {
+        ...this.state.book,
+        [event.target.name]: event.target.checked
+      };
+    } else {
+      newBook = {
+        ...this.state.book,
+        [event.target.name]: event.target.value
+      };
+    }
+
+    this.setState({
+      book: newBook
+    });
+  };
+
   render() {
     return (
       <div className="adminPanel col-md-4">
@@ -12,6 +45,8 @@ class AdminPanel extends React.Component {
               id="bookName"
               name="bookName"
               className="form-control"
+              onChange={this.handleChange}
+              value={this.state.book.name}
             />
           </div>
           <div className="form-group">
@@ -21,6 +56,8 @@ class AdminPanel extends React.Component {
               id="bookAuthor"
               name="bookAuthor"
               className="form-control"
+              onChange={this.handleChange}
+              value={this.state.book.author}
             />
           </div>
           <div className="form-group">
@@ -29,6 +66,8 @@ class AdminPanel extends React.Component {
               id="bookDescription"
               name="bookDescription"
               className="form-control"
+              onChange={this.handleChange}
+              value={this.state.book.description}
             />
           </div>
           <div className="form-group">
@@ -37,6 +76,8 @@ class AdminPanel extends React.Component {
               id="bookOnStock"
               name="bookOnStock"
               className="form-check-input"
+              onChange={this.handleChange}
+              checked={this.state.book.onStock}
             />
             <label htmlFor="bookOnStock" className="form-check-label">
               On stock
@@ -49,6 +90,8 @@ class AdminPanel extends React.Component {
               id="bookImage"
               name="bookImage"
               className="form-control"
+              onChange={this.handleChange}
+              value={this.state.book.image}
             />
           </div>
           <button type="submit" className="btn btn-primary">
